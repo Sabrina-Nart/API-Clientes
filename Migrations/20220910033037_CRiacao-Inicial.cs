35f05d7632540d0b9727c726fb9bf4b9bab36ef3
Sabrina-Nart <84355782+Sabrina-Nart@users.noreply.github.com>
2022-09-11 19:37:27 -0300

    Add files via upload

---
+++ Migrations/20220910033037_CRiacao-Inicial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebPostgreSQL.Migrations
+{
+    public partial class CRiacaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CLIENTES",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    CNPJ = table.Column<string>(type: "text", nullable: true),
+                    DataCadastro = table.Column<DateOnly>(type: "timestamp with time zone", nullable: true),
+                    Endereco = table.Column<string>(type: "text", nullable: true),
+                    Telefone = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CLIENTES", x => x.Codigo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CLIENTES");
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+
+namespace WebPostgreSQL.Models
+{
+    [Table("CLIENTES")]
+    public class Cliente
+    {
+        [Key]
+        [Column("Codigo")]
+        [Display(Name = "Código")]
+        public int CodigoCliente { get; set; }
+
+        [Required(ErrorMessage ="Nome Obrigatório")]
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public String? Nome { get; set; }
+
+        [Required(ErrorMessage ="CNPJ Obrigatório")]
+        [Column("CNPJ")]
+        [Display(Name = "CNPJ")]
+        public String? CNPJ { get; set; }
+
+        [Column("DataCadastro")]
+        [Display(Name = "Data de Cadastro")]
+        public DateOnly? DataCadastro { get; set; }
+
+        [Required(ErrorMessage = "Endereço Obrigatório")]
+        [Column("Endereco")]
+        [Display(Name = "Endereço")]
+        public String? Endereco { get; set; }
+
+        [Required(ErrorMessage = "Telefone Obrigatório")]
+        [Column("Telefone")]
+        [Display(Name = "Telefone")]
+        public string? Telefone { get; set; }
+        
+    }
+}
